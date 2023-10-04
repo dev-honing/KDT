e51e7b7644ea9aff09b330dadc68b7f9b685e004
Byeon Ho Nyeong <dev.honing@gmail.com>
2023-10-04 15:26:15 +0900

    서버 만드는 메서드 createServer() 사용

--- Develop-12.js
+++ Develop-12.js
@@ -2,3 +2,6 @@
 // 프로젝트 파일 시스템에 접근하기 위해 fs 모듈을 import
 const http = require("http");
 const fs = require("fs");
+
+// 서버 만드는 대표적 메서드인 createServer() 사용
+http.createServer(function (request, response) {});
