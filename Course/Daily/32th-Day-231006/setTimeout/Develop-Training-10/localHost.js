c778406e2a066dcce5337d25743002eb810a41b3
Byeon Ho Nyeong <dev.honing@gmail.com>
2023-10-06 14:49:42 +0900

    포트번호를 8080으로 설정

--- Course/Daily/32th-Day-231006/setTimeout/Develop-Training-10/localHost.js
+++ Course/Daily/32th-Day-231006/setTimeout/Develop-Training-10/localHost.js
@@ -1,15 +1,17 @@
 const http = require("http");
 const fs = require("fs");
-http.createServer(function (request, response) {
-  console.log(request.method);
-  console.log(request.url);
-  let writeHeadObject = { "Content-Type": "text/html" };
-  response.writeHead(200, writeHeadObject);
-  fs.readFile("./clickGame.html", function (err, data) {
-    if (err) {
-      console.error("파일을 읽지 못했습니다.");
-    } else {
-      response.end(data);
-    }
-  });
-});
+http
+  .createServer(function (request, response) {
+    console.log(request.method);
+    console.log(request.url);
+    let writeHeadObject = { "Content-Type": "text/html" };
+    response.writeHead(200, writeHeadObject);
+    fs.readFile("./clickGame.html", function (err, data) {
+      if (err) {
+        console.error("파일을 읽지 못했습니다.");
+      } else {
+        response.end(data);
+      }
+    });
+  })
+  .listen(8080);
