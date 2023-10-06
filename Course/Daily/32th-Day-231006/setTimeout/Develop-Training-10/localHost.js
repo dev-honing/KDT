2d61a13aa86d68ccd9b1384d622b6ed2e15b6ca3
Byeon Ho Nyeong <dev.honing@gmail.com>
2023-10-06 14:48:24 +0900

    response 부분의 로직 작성

--- Course/Daily/32th-Day-231006/setTimeout/Develop-Training-10/localHost.js
+++ Course/Daily/32th-Day-231006/setTimeout/Develop-Training-10/localHost.js
@@ -3,4 +3,6 @@ const fs = require("fs");
 http.createServer(function (request, response) {
   console.log(request.method);
   console.log(request.url);
+  let writeHeadObject = { "Content-Type": "text/html" };
+  response.writeHead(200, writeHeadObject);
 });
