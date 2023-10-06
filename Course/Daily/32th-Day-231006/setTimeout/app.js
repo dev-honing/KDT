c5fc89b41f605f91e9fa16410b1fb17ffc468469
Byeon Ho Nyeong <dev.honing@gmail.com>
2023-10-06 11:10:24 +0900

    동기적 처리와 비동기적 처리

--- Course/Daily/32th-Day-231006/setTimeout/app.js
+++ Course/Daily/32th-Day-231006/setTimeout/app.js
@@ -1,11 +1,12 @@
 setTimeout(function () {
   console.log("첫번째");
-  setTimeout(function () {
-    console.log("두번째");
-    setTimeout(function () {
-      console.log("세번째");
-    }, 1000);
-  }, 2000);
 }, 3000);
-// 함수를 부르는 함수 : 콜백함수
-// 제어할 수 있지만 이렇게 많이 사용하면 "Callback Hell"이 생길 수 있다!
+setTimeout(function () {
+  console.log("두번째");
+}, 2000);
+setTimeout(function () {
+  console.log("세번째");
+}, 1000);
+
+// 작업 단위 실행 : 동기적 처리 (순서대로)
+// 시간 단위 실행 : 비동기적 처리
