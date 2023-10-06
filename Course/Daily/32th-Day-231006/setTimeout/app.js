89b98699b780a2d9e8b18c04844621d844e5640e
Byeon Ho Nyeong <dev.honing@gmail.com>
2023-10-06 10:48:03 +0900

    두번째가 5초 뒤에 실행되도록 할 수 있다 - 콜백함수 응용

--- Course/Daily/32th-Day-231006/setTimeout/app.js
+++ Course/Daily/32th-Day-231006/setTimeout/app.js
@@ -1,10 +1,10 @@
 setTimeout(function () {
   console.log("첫번째");
+  setTimeout(function () {
+    console.log("두번째");
+  }, 2000);
 }, 3000);
-setTimeout(function () {
-  console.log("두번째");
-}, 2000);
 setTimeout(function () {
   console.log("세번째");
 }, 1000);
-// 시간에 따른 순서를 지정할 수 있다는 게 포인트!
+// 한 줄만 위로 올리면 "두번째"가 5초 뒤에 실행되게 할 수 있다!
