40c305604bfd316387fa702f0218c50dae61e7fd
Byeon Ho Nyeong <dev.honing@gmail.com>
2023-10-06 11:31:59 +0900

    자기자신을 부르는 재귀함수 - Self Reference Recursion을 반복하면 Overflow를 야기

--- Course/Daily/32th-Day-231006/setTimeout/app.js
+++ Course/Daily/32th-Day-231006/setTimeout/app.js
@@ -1,12 +1,9 @@
-setTimeout(function () {
-  console.log("첫번째");
-}, 3000);
-setTimeout(function () {
-  console.log("두번째");
-}, 2000);
-setTimeout(function () {
-  console.log("세번째");
-}, 1000);
-
-// 작업 단위 실행 : 동기적 처리 (순서대로)
-// 시간 단위 실행 : 비동기적 처리
+// 재귀함수 생성
+function Recursion() {
+  setTimeout(function () {
+    console.log("repeat");
+    Recursion();
+  }, 1000);
+}
+// 재귀함수 호출
+Recursion();
