8df56bb45e10106478e6b47b5f3843b5936f6b33
Byeon Ho Nyeong <dev.honing@gmail.com>
2023-10-06 10:38:10 +0900

    3초 후 3가지 콘솔이 찍히는지 확인

--- Course/Daily/32th-Day-231006/setTimeout/app.js
+++ Course/Daily/32th-Day-231006/setTimeout/app.js
@@ -1,3 +1,9 @@
 setTimeout(function () {
-  console.log("test");
+  console.log("첫번째");
+}, 3000);
+setTimeout(function () {
+  console.log("두번째");
+}, 3000);
+setTimeout(function () {
+  console.log("세번째");
 }, 3000);
