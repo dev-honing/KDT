b6dae79ed8ecd6350ce2c93939be25071467bdab
Tech1997 <dev.honing@gmail.com>
2023-10-13 15:29:18 +0900

    요청 메서드 방식을 콘솔에서 확인

--- Assignment/Subject-4/Lotto/localhost.js
+++ Assignment/Subject-4/Lotto/localhost.js
@@ -1,3 +1,7 @@
 // http 모듈 import
 const http = require("http");
-http.createServer((request, responsd) => {})
+http.createServer((request, responsd) => {
+  console.log(request.method); // GET 방식을 사용한 것을 콘솔에서 확인
+  console.log(request.url);
+})
+.listen(8080);
