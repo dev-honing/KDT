64a26a360e04049a55003a058d639c51e5a2f046
Tech1997 <dev.honing@gmail.com>
2023-10-13 15:36:56 +0900

    fs 모듈 import

--- Assignment/Subject-4/Lotto/localhost.js
+++ Assignment/Subject-4/Lotto/localhost.js
@@ -1,5 +1,9 @@
 // http 모듈 import
 const http = require("http");
+
+// fs 모듈 import
+const fs = require("fs");
+
 http.createServer((request, response) => {
   console.log(request.url);
   const writeHeadObject = {'Content-Type' : 'text/html'}
