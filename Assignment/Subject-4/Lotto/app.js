d7014681af8277b97ac8736b42d7d30171db55ac
Tech1997 <dev.honing@gmail.com>
2023-10-16 15:28:25 +0900

    배열을 정렬하기 위한 탐구-array.sort() 메서드

--- Assignment/Subject-4/Lotto/app.js
+++ Assignment/Subject-4/Lotto/app.js
@@ -1,20 +1,2 @@
-// entry 파일
-
-// 번호 추첨을 위한 로직 작성
-function conArr(){
-  const newArr = [];
-  for (i=0; i<3; i++){
-  randomNum = Math.floor(Math.random()*15+1);
-  
-  if(newArr.indexOf(randomNum) === -1 ){ // ? 조건식 부분을 잘 모르겠다
-    newArr.push(randomNum);
-  } else {
-    i--}
-  }
-  console.clear();
-  console.count();
-  console.log(newArr);
-}
-setInterval(conArr, 1000); // [14, 11, 9] 등 정렬이 안되는 문제 확인!
-
-// 보기 좋게 순차 정렬 해보자!
+// Array.sort() 메서드 활용
+const arr = [1, 2, 3];
