15128fc03602337deb1a2821594f1b10a4c29ac6
Tech1997 <dev.honing@gmail.com>
2023-10-16 16:45:42 +0900

    array.sort() 메서드는 문자열 정렬하는 기법이었다

--- Assignment/Subject-4/Lotto/app.js
+++ Assignment/Subject-4/Lotto/app.js
@@ -10,14 +10,21 @@ function conArr(){
     const randomNum = Math.floor(Math.random()*15+1);
     if(newArr.indexOf(randomNum) === -1){
       newArr.push(randomNum);
+      
     } else {
       i--;
     }
   }
   console.clear();
   console.count();
-  // console.log(newArr);
-  const sortNewArr = newArr.sort();
-  console.log(sortNewArr);
+  const SortednewArr = newArr.sort();
+  console.log({newArr, SortednewArr});
 }
 setInterval(conArr, 1000);
+
+// ! array.sort() 메서드는 문자열을 정렬하는 기법이라 40과 100을 비교하면 4가 더 크므로 40이 더 큰 것으로 판단한다고 한다.
+// 증명과정
+const a = [40, 100];
+console.log(a.sort()); // [100, 40]
+console.log(a.reverse()); // [40, 100]
+// 진짜였다...
