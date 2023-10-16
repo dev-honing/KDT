1bd0df89aa3bd1bfec06d99cd7d44c36032ce5a0
Tech1997 <dev.honing@gmail.com>
2023-10-17 07:16:39 +0900

    요청 URL과 요청 메서드 확인을 위한 콘솔 출력

--- Classroom/Network/Network-1/request-and-route.js
+++ Classroom/Network/Network-1/request-and-route.js
@@ -6,4 +6,9 @@ const fs = require('fs');
 http.createServer((req, res) =>{
   res.writeHead(500); // 서버 에러 응답 코드
   return res.end('500! Internal Server Error, 서버에 예상치 못한 문제가 생겼습니다.');
-});
+}
+);
+console.log("어떤 요청이 들어오는지 확인", "url -> ", req.url, "method -> ", req.method);
+// 라우팅 처리 제작할 때, 두 가지 요청 데이터를 확인해야 한다.
+// 1. 요청 URL
+// 2. 요청 메서드
