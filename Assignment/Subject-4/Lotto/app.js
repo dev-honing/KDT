725588ce9a250e963cc8420558dda44ce2e76c02
Tech1997 <dev.honing@gmail.com>
2023-10-16 13:03:54 +0900

    중복된 요소를 뽑지 않는 문제 발생

--- Assignment/Subject-4/Lotto/app.js
+++ Assignment/Subject-4/Lotto/app.js
@@ -27,12 +27,14 @@ const arr = [1, 2, 3];
 
 
 // 참조(https://velog.io/@eunjin/Javascript-%ED%8A%B9%EC%A0%95-%EC%88%AB%EC%9E%90-%EB%B2%94%EC%9C%84%EC%97%90%EC%84%9C-%EC%A4%91%EB%B3%B5%EC%9D%84-%EC%A0%9C%EC%99%B8%ED%95%9C-%EB%9E%9C%EB%8D%A4-%EC%88%AB%EC%9E%90-%EB%BD%91%EB%8A%94-%EB%B0%A9%EB%B2%95)
-// 새로운 관점 - 빈 배열을 변수 선언하고, for문에 넣은 뒤, arr.push() 메서드로 요소를 추가해줌
-const newArr = []; // 빈 배열 선언
-// 3가지 숫자를 뽑음
-for(i=0; i<3; i++){
-  randomNum = Math.floor(Math.random()*15+1); // 1~15 범위의 숫자가 생성됨
-  newArr.push(randomNum); // arr.push() 메서드를 이용해 배열에 요소 추가
+
+// 중복을 피하기 위해 요소를 확인하고 없으면 넣어준다
+const newArr = [];
+for (i=0; i<3; i++){
+  randomNum = Math.floor(Math.random()*15+1);
+  // randomNum이 newArr에 없을 때 쓰는 조건식
+  if(newArr.indexOf(randomNum) === -1 ){
+    newArr.push(randomNum); // randomNum을 newArr 배열에 추가
+  }
 }
-console.log(newArr); // 요소가 추가된 배열을 출력
-console.log(newArr.indexOf(16)); // arr.indexOf() 메서드는 요소가 배열에 없으면 '-1'을 출력한다.
+console.log(newArr); // ? 중복된 요소가 있으면 뽑지를 않는 문제 발생!
