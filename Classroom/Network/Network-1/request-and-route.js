f7f77f9410dd054b9a35b7dcb44cc582141a9fa8
Tech1997 <dev.honing@gmail.com>
2023-10-17 07:11:50 +0900

    서버 에러 응답 코드 작성

--- Classroom/Network/Network-1/request-and-route.js
+++ Classroom/Network/Network-1/request-and-route.js
@@ -4,4 +4,6 @@ const fs = require('fs');
 
 // createServer() 메서드로 서버 만들기
 http.createServer((req, res) =>{
+  res.writeHead(500); // 서버 에러 응답 코드
+  return res.end('500! Internal Server Error, 서버에 예상치 못한 문제가 생겼습니다.');
 });
