a812d3c49b37e7360b4f6b9ca3940be1daac8e7e
Tech1997 <dev.honing@gmail.com>
2023-10-16 09:37:15 +0900

    css 불러오기 시도해보기 - 모두 실패

--- Assignment/Subject-4/Lotto/cssRead.js
+++ Assignment/Subject-4/Lotto/cssRead.js
@@ -2,33 +2,35 @@ const http = require('http');
 const fs = require('fs');
 
 http.createServer((req, res) => {
-  const htmlData = fs.readFileSync("index.html", "utf-8");
-  const cssData = fs.readFileSync("output-style.css", "utf-8");
-
+  
   if (req.url === '/') {
-    if (res.statusCode === 200){
-      console.log("정상적인 접근입니다.");
-    }
+    if (res.statusCode !== 200){
+      console.error("접속 오류!");
+    } 
     else {
-      console.error("접속 에러");
+      console.log("정상적인 접근입니다.");
+      function typeHTML() { 
+        res.writeHead(200, {'Content-Type' : 'text/html'});
+        const htmlData = fs.readFile("index.html", "utf-8");
+        res.write(htmlData)};
+        setTimeout(typeHTML, 1000);
+        
+        function typeCSS() { 
+          res.writeHead(200, {'Content-Type': 'text/css'});
+          const cssData = fs.readFile("output-style.css", "utf-8");
+        res.end(cssData)};
+      setTimeout(typeCSS, 2000);
     }
   }
-  if (req.url === '/start'){
-    if (res.statusCode === 200) {
-      console.log("서브 페이지 접속");
-    } else {
-      console.error("접속 에러");
-    }
-  }
+  // if (req.url === '/start'){
+  //   if (res.statusCode === 200) {
+  //     res.writeHead(200, { 'Content-Type': 'text/html' })
+  //     // res.write(htmlData);
+  //     res.end(cssData);
+  //   } else {
+  //     console.error("접속 에러");
+  //   }
+  // }
 })
 .listen(3000, () => {
-  console.log('Server running on port 3000')})
-
-  
-  
-  
-    // res.writeHead(200, { 'Content-Type': 'text/html' });
-    // res.write(htmlData);
-    // res.end(cssData);
-
-    
+console.log('Server running on port 3000')})
