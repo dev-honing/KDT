0d4ea63fea2ce730334c45156fd7061df2cf1b35
Tech1997 <dev.honing@gmail.com>
2023-10-16 16:55:23 +0900

    잘 작동하는지 확인-성공

--- Assignment/Subject-4/Lotto/app.js
+++ Assignment/Subject-4/Lotto/app.js
@@ -23,14 +23,3 @@ function conArr(){
   console.log(newArr);
 }
 setInterval(conArr, 1000);
-
-// ! array.sort() 메서드는 문자열을 정렬하는 기법이라 40과 100을 비교하면 4가 더 크므로 40이 더 큰 것으로 판단한다고 한다.
-// 증명과정
-const a = [40, 100];
-
-// 정렬을 위해 뺄셈식을 작성한다.
-
-a.sort(function(a,b){
-  return a-b;
-})
-console.log(a);
