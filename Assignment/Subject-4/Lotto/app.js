529ca1e3a357b868772dafc2eaadac8bee498983
Tech1997 <dev.honing@gmail.com>
2023-10-16 15:42:47 +0900

    arr.sort() 메서드와 arr.reverse() 메서드 알아보기

--- Assignment/Subject-4/Lotto/app.js
+++ Assignment/Subject-4/Lotto/app.js
@@ -1,4 +1,21 @@
 // Array.sort() 메서드 활용
 const arr = [3, 2, 1];
-const sortArr = arr.sort();
-console.log(sortArr); // [1, 2, 3]
+
+console.log(arr.sort());
+console.log(arr.reverse());
+// Math.floor(Math.random()) 메서드도 되는지 확인
+function conArr(){
+  const newArr = [];
+  for (i=0; i<3; i++){
+    const randomNum = Math.floor(Math.random()*15+1);
+    if(newArr.indexOf(randomNum) === -1){
+      newArr.push(randomNum);
+    } else {
+      i--;
+    }
+  }
+  // console.clear();
+  // console.count();
+  // console.log(Array.sort(newArr));
+}
+setInterval(conArr, 1000);
