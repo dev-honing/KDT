129b4911cfbaa0c56fc767adcf99b9aad0317435
Tech1997 <dev.honing@gmail.com>
2023-10-17 10:24:16 +0900

    요청 내용을 server로 변수 선언하고 포트 번호로 3000을 부여

--- Classroom/Network/Network-2/request.js
+++ Classroom/Network/Network-2/request.js
@@ -1,10 +1,13 @@
 // http 모듈 import
 const http = require('http');
 
-http.createServer((req, res)=>{
+const server = http.createServer((req, res)=>{
   console.log("URL로 최초접속하는 트랜잭션 확인하기");
   console.log(req.url);
 
   console.log("URL로 최초접속하는 요청 유형 확인하기")
   console.log(req.method);
 });
+server.listen(3000, () =>{
+  console.log(`server running : http://localhost:3000/`);
+})
