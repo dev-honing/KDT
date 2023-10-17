c9bf664767ffbd8f4bc4c5a8b5a971e56ed75cc0
Tech1997 <dev.honing@gmail.com>
2023-10-17 10:57:11 +0900

    요청 메서드 확인하는 내용을 삭제해보고 `git diff`로 확인

--- Classroom/Git/Git-2/git diff.js
+++ Classroom/Git/Git-2/git diff.js
@@ -8,8 +8,6 @@ const server = http.createServer((req, res)=>{
   console.log("URL로 최초접속하는 트랜잭션 확인하기");
   console.log(req.url);
 
-  console.log("URL로 최초접속하는 요청 유형 확인하기")
-  console.log(req.method);
 });
 server.listen(3000, () =>{
   console.log(`server running : http://localhost:3000/`);
