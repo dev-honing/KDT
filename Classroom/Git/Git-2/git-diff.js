86e5364b09512e8eac09506a6873b512c1656766
Tech1997 <dev.honing@gmail.com>
2023-10-17 11:02:06 +0900

    git-diff.js로 파일 이름 변경하고 `git diff` 명령어에 대해 알게된 내용 정리

---
+++ Classroom/Git/Git-2/git-diff.js
@@ -0,0 +1,18 @@
+// git diff.js
+// Network-2에서 작업한 예제를 재사용
+
+// http 모듈 import
+const http = require('http');
+
+const server = http.createServer((req, res)=>{
+  console.log("URL로 최초접속하는 트랜잭션 확인하기");
+  console.log(req.url);
+
+});
+server.listen(3000, () =>{
+  console.log(`server running : http://localhost:3000/`);
+})
+// 요청 URL은 '/'이고 요청 유형은 'GET'임을 확인!
+
+// ! git diff는 Working Directory와 Staging Area 사이의 차이를 확인하기 위한 명령어이므로 커밋을 한 뒤, `git diff`를 쓰면 아무것도 확인되지 않았다.
+// * 추가 사항은 초록색으로, 삭제된 사항은 빨간색으로 표기되는 것을 확인
