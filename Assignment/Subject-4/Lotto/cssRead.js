1742fd8f1872cf530c9904d4a8b9d7f0a5180c85
Tech1997 <dev.honing@gmail.com>
2023-10-17 15:35:36 +0900

    css 불러오기에 실패했던 이유 리뷰

--- Assignment/Subject-4/Lotto/cssRead.js
+++ Assignment/Subject-4/Lotto/cssRead.js
@@ -2,7 +2,26 @@ const http = require('http');
 const fs = require('fs');
 
 const server = http.createServer((req, res) => {
-  
+  // 요청 URL이 /이고, 요청 메서드가 GET일 때
+  if (req.url === '/' && req.method === 'GET'){
+    fs.readFile('./index.html', 'utf-8', (err, data) => {
+      if (err) {
+        console.log("오류입니다.");
+      }
+      res.writeHead(200, {'Content-Type' : 'text/html'});
+      res.end(data);
+    })
+  }
+
+
+
+
+
+  // ? 내가 이전에 하려던 방식을 분석하기
+  // 무엇이 틀렸나?
+  // ! 스타일 시트를 link까진 했지만 응답을 조회해서 HTML이 스타일 시트를 가져오는 URL을 요청했을 때의 대응을 작성하지 않았다!
+  // 때문에 css를 요청함이 콘솔에는 찍혔지만 css를 불러오지는 못했던 것이었다.
+   
   if (req.url === '/') {
     if (res.statusCode !== 200){
       console.error("접속 오류!");
