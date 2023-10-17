f20657dae6d064d737d05744080cc345b550a3e8
Tech1997 <dev.honing@gmail.com>
2023-10-17 14:36:31 +0900

    <link>로 css를 연결하고 요청 URL을 확인한 후, 조건식의 req.url을 변경해주고 연결된 것을 확인

--- Classroom/Network/Network-1/request-and-route.js
+++ Classroom/Network/Network-1/request-and-route.js
@@ -28,7 +28,7 @@ const server = http.createServer((req, res) =>{
       res.end(data);
     });
     // else if 조건식: 요청 URL이 css이고 요청 메서드가 GET일 때
-  } else if (req.url === 'css/style.css' & req.method === 'GET'){
+  } else if (req.url === '/style.css' & req.method === 'GET'){
     fs.readFile('./style.css','utf-8', (err, data) => {
       if (err) {
         serverErrorLog();
@@ -36,7 +36,7 @@ const server = http.createServer((req, res) =>{
       res.writeHead(200, {'Content-Type': 'text/css'});
       res.end(data);
     });
-} else if (req.url === '/js/app.js' && req.method === 'GET') {
+} else if (req.url === '/app.js' && req.method === 'GET') {
   fs.readFile('./app.js', 'utf-8', (err, data) => {
     if (err) {
       serverErrorLog();
