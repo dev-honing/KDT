6f3b5ea57d816497c173a634933e23a54a330045
Tech1997 <dev.honing@gmail.com>
2023-10-18 11:35:15 +0900

    디버거에 출력되는 사항 정리

--- Classroom/Network/Network-4/request-POST.js
+++ Classroom/Network/Network-4/request-POST.js
@@ -1,6 +1,10 @@
 const object = {a: 1, b: 2};
 console.log(object); // { a: 1, b: 2 }
 
+// 중단점을 찍고 디버그를 실행해 변동사항을 확인
+// a: undefined, b: undefined
+// object: {a: 1, b: 2}
+
 const {a, b} = object;
 console.log({a,b}); // { a: 1, b: 2 } 
 // ? 
