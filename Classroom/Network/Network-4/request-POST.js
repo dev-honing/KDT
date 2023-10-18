6aa3005e8511c63c17721b0d6e5ca1dcd648e4c1
Tech1997 <dev.honing@gmail.com>
2023-10-18 11:09:01 +0900

    오자 수정 - 두 가지 모두 { a: 1, b: 1 }가 출력됨

--- Classroom/Network/Network-4/request-POST.js
+++ Classroom/Network/Network-4/request-POST.js
@@ -1,5 +1,5 @@
 const object = {a: 1, b: 1};
-console.log(object); // { a: 1, b: 1}
+console.log(object); // { a: 1, b: 1 }
 
 const {a, b} = object;
-console.log({a,b}); // { a: 1, b: 1}
+console.log({a,b}); // { a: 1, b: 1 }
