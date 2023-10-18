e4c9ca1ac81193c3359b312ece1509487074e49b
Tech1997 <dev.honing@gmail.com>
2023-10-18 11:38:43 +0900

    비구조화 할당을 하고 디버거 확인

--- Classroom/Network/Network-4/request-POST.js
+++ Classroom/Network/Network-4/request-POST.js
@@ -1,11 +1,14 @@
 const object = {a: 1, b: 2};
 console.log(object); // { a: 1, b: 2 }
 
-// 중단점을 찍고 디버그를 실행해 변동사항을 확인
+// 디버거 확인
 // a: undefined, b: undefined
 // object: {a: 1, b: 2}
 
 const {a, b} = object;
 console.log({a,b}); // { a: 1, b: 2 } 
-// ? 
-// -> {a, b}를 출력하면 왜 object가 뜨지 않나?
+
+// 디버거 확인
+// a: 1, b: 2
+// object: {a: 1, b: 2}
+// ! a와 b의 value가 생겼음
