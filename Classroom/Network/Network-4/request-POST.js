2b744a5d40c6145c032986e907d3f960ddb99718
Tech1997 <dev.honing@gmail.com>
2023-10-18 11:13:55 +0900

    b의 value를 2로 수정

--- Classroom/Network/Network-4/request-POST.js
+++ Classroom/Network/Network-4/request-POST.js
@@ -1,11 +1,7 @@
-const object = {a: 1, b: 1};
-console.log(object); // { a: 1, b: 1 }
+const object = {a: 1, b: 2};
+console.log(object); // { a: 1, b: 2 }
 
 const {a, b} = object;
-console.log({a,b}); // { a: 1, b: 1 } 
+console.log({a,b}); // { a: 1, b: 2 } 
 // ? 
 // -> {a, b}를 출력하면 왜 object가 뜨지 않나?
-
-// key로 출력
-console.log(a); // 1
-console.log(b); // 1
