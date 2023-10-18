9fc92a210269ba31c5a31014b2e57e3430e1f83d
Tech1997 <dev.honing@gmail.com>
2023-10-18 11:11:52 +0900

    의문점 기록

--- Classroom/Network/Network-4/request-POST.js
+++ Classroom/Network/Network-4/request-POST.js
@@ -2,4 +2,6 @@ const object = {a: 1, b: 1};
 console.log(object); // { a: 1, b: 1 }
 
 const {a, b} = object;
-console.log({a,b}); // { a: 1, b: 1 }
+console.log({a,b}); // { a: 1, b: 1 } 
+// ? 
+// -> {a, b}를 출력하면 왜 object가 뜨지 않나?
