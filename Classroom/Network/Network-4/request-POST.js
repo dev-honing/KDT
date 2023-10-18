5516d30dec4eee99349cf5f3756c6715a742ecf2
Tech1997 <dev.honing@gmail.com>
2023-10-18 10:01:20 +0900

    조건식 작성

--- Classroom/Network/Network-4/request-POST.js
+++ Classroom/Network/Network-4/request-POST.js
@@ -4,5 +4,8 @@ const http = require("http");
 const querystring = require("querystring");
 
 http.createServer((req, res) => {
-  
+  // 조건식 작성 - POST 방식이고 URL 요청이 /login이면,
+  if (req.method === "POST" && req.url === "/login") {
+    
+  }
 })
