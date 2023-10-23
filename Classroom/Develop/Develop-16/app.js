8c87a919adab1bd911fe7155a2f22451a5796a6d
Tech1997 <dev.honing@gmail.com>
2023-10-23 22:02:36 +0900

    콘솔에 출력시켜 확인해보기

--- Classroom/Develop/Develop-16/app.js
+++ Classroom/Develop/Develop-16/app.js
@@ -11,3 +11,6 @@ const { pokemonName, type } = _pokemon;
 // 배열이나 객체의 속성을 해체하여 그 값을 개별 변수에 담을 수 있게 하는 표현식
 // (객체 접근 방식을 채택해서 작성함)
 // JavaScript뿐만 아니라, 다른 언어에서도 많이 사용하는 방식
+
+console.log(_pokemon.pokemonName); // 피카츄
+console.log(_pokemon.type); // 전기
